2c09ceac67aa3f1b31687feea93193163aff6065
Jon-Muniay <jmuniayhij@educacion.navarra.es>
2025-04-16 17:21:39 +0200

    Delete untitled directory

--- untitled/src/main/java/org/example/Calculadora/test.java
+++
@@ -1,4 +0,0 @@
-package org.example.Calculadora;
-
-public class test {
-}
--- untitled/src/test/java/org/example/Calculadora/testTest.java
+++
@@ -1,7 +0,0 @@
-package org.example.Calculadora;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class testTest {
-
-}
